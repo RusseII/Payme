16a9afb4b53f0bf0ec253f5b12aa5aded7897279
Russell <russell@deephire.com>
2022-01-10 15:14:08 -0800

    convert to typescript

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+import { ethers } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Payments = await hre.ethers.getContractFactory("Payments");
-  const greeter = await Payments.deploy();
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
   await greeter.deployed();
 
-  console.log("Payments deployed to:", greeter.address);
+  console.log("Greeter deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,9 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+import { expect } from "chai";
+import { ethers } from "hardhat";
 
 describe('Payments', function () {
   it('Should allow sending money', async function () {
+    const [owner] = await ethers.getSigners()
     const Payments = await ethers.getContractFactory('Payments');
     const greeter = await Payments.deploy();
     await greeter.deployed();
