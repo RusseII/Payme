c6f003604b5c5df374582fbec01eed088f599ee9
bookland <bookland.porter.finance>
2022-01-13 06:37:07 -0800

    fix deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Payments = await ethers.getContractFactory("Payments");
+  const greeter = await Payments.deploy();
 
   await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Payments deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
